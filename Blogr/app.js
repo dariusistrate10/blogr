25f7de2f11b4b607b99668587945d24e5bdce60d
dariusistrate10 <101264872+dariusistrate10@users.noreply.github.com>
2023-04-16 15:39:48 +0300

    Add files via upload

---
+++ Blogr/app.js
@@ -0,0 +1,8 @@
+ScrollReveal({ 
+    reset: true ,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+ScrollReveal().reveal('header .header-text h1', { delay: 100, origin: 'left'});
